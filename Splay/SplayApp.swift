f5a3f43d10229825f1495796dd38fb51917487a5
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-06-09 12:16:43 +0900

    Initial Commit

---
+++ Splay/SplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplayApp.swift
+//  Splay
+//
+//  Created by Aybars Nazlica on 2025/06/09.
+//
+
+import SwiftUI
+
+@main
+struct SplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SplayTests/SplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  SplayTests.swift
+//  SplayTests
+//
+//  Created by Aybars Nazlica on 2025/06/09.
+//
+
+import Testing
+@testable import Splay
+
+struct SplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
