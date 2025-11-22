d4746ca614c5df7e58eb19a0e19e677be3f7c82f
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-11-22 21:58:00 +0900

    refactor: remove unnecessary comments

--- Splay/Sources/Splay/ContentView.swift
+++ Splay/Sources/Splay/ContentView.swift
@@ -8,14 +8,7 @@
 import SwiftUI
 
 
-/// The main content view of the Splay app's popover UI.
-///
-/// Displays buttons for triggering the window "splay" action and quitting the application.
 struct ContentView: View {
-    /// The content and layout of the view.
-    ///
-    /// Provides a vertical stack containing two buttons: one to invoke window splaying
-    /// and another to terminate the application.
     @State private var scale = 5
     
     var body: some View {
--- Splay/Sources/Splay/SplayApp.swift
+++ Splay/Sources/Splay/SplayApp.swift
@@ -7,15 +7,11 @@
 
 import SwiftUI
 
-/// The main entry point of the Splay application.
-///
-/// Configures the app's delegate and settings scene. Uses `@main` attribute to indicate the starting point of the app.
+
 @main
 struct SplayApp: App {
-    /// The application delegate used to handle app-level events and UI setup.
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-    /// The main scene of the app, which in this case provides an empty Settings view.
     var body: some Scene {
         Settings {
             EmptyView()
